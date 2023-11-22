c7a50fe0f8090d0f5a832ac4379a63f1f3ba080b
Kristóf Kálai <kristof.kalai@supercharge.io>
2023-11-23 00:50:20 +0100

    feat: update with continuous observation

---
+++ Sources/SizeUpdateStrategy.swift
@@ -0,0 +1,13 @@
+//
+//  SizeUpdateStrategy.swift
+//
+//
+//  Created by Kristóf Kálai on 23/11/2023.
+//
+
+public enum SizeUpdateStrategy {
+    case once
+
+    @available(iOS 15, *)
+    case always
+}
--- Sources/View+Extensions/View+Extensions.swift
+++ Sources/View+Extensions/View+Extensions.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 extension View {
-    public func readProxy(onChange: @escaping (GeometryProxy) -> Void) -> some View {
-        background(
+    public func readProxy(onAppear: @escaping (GeometryProxy) -> Void) -> some View {
+        overlay(
             GeometryReader { proxy in
                 Color.clear
                     .onAppear {
-                        onChange(proxy)
+                        onAppear(proxy)
                     }
             }
         )
@@ -21,87 +21,166 @@ extension View {
 }
 
 extension View {
-    public func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
-        readProxy { onChange($0.size) }
+    public func readSize(
+        update strategy: SizeUpdateStrategy = .once,
+        onChange: @escaping (CGSize) -> Void
+    ) -> some View {
+        overlay(
+            GeometryReader { proxy in
+                let proxySize = proxy.size
+
+                let observer = Color.clear
+                    .onAppear {
+                        guard strategy == .once else { return }
+                        onChange(proxy.size)
+                    }
+
+                if #available(iOS 15, *) {
+                    observer
+                        .task(id: proxy.size) {
+                            guard strategy == .always else { return }
+                            await MainActor.run {
+                                onChange(.init(width: max(0, proxySize.width), height: max(0, proxySize.height)))
+                            }
+                        }
+                } else {
+                    observer
+                }
+            }
+        )
     }
 
-    public func readHeight(onChange: @escaping (CGFloat) -> Void) -> some View {
-        readSize { onChange($0.height) }
+    public func readHeight(
+        update strategy: SizeUpdateStrategy = .once,
+        onChange: @escaping (CGFloat) -> Void
+    ) -> some View {
+        readSize(update: strategy) { onChange($0.height) }
     }
 
-    public func readWidth(onChange: @escaping (CGFloat) -> Void) -> some View {
-        readSize { onChange($0.width) }
+    public func readWidth(
+        update strategy: SizeUpdateStrategy = .once,
+        onChange: @escaping (CGFloat) -> Void
+    ) -> some View {
+        readSize(update: strategy) { onChange($0.width) }
     }
 }
 
 extension View {
-    public func storeSize(in size: Binding<CGSize>, block: @escaping (CGSize) -> Void = { _ in }) -> some View {
-        readSize {
+    public func storeSize(
+        update strategy: SizeUpdateStrategy = .once,
+        in size: Binding<CGSize>,
+        block: @escaping (CGSize) -> Void = { _ in }
+    ) -> some View {
+        readSize(update: strategy) {
             size.wrappedValue = $0
             block($0)
         }
     }
 
-    public func storeSize(in size: Binding<CGSize?>, block: @escaping (CGSize) -> Void = { _ in }) -> some View {
-        readSize {
+    public func storeSize(
+        update strategy: SizeUpdateStrategy = .once,
+        in size: Binding<CGSize?>,
+        block: @escaping (CGSize) -> Void = { _ in }
+    ) -> some View {
+        readSize(update: strategy) {
             size.wrappedValue = $0
             block($0)
         }
     }
 
-    public func storeSize(in size: Binding<CGSize>, block: @escaping () -> Void) -> some View {
-        storeSize(in: size, block: { _ in block() })
+    public func storeSize(
+        update strategy: SizeUpdateStrategy = .once,
+        in size: Binding<CGSize>,
+        block: @escaping () -> Void
+    ) -> some View {
+        storeSize(update: strategy, in: size, block: { _ in block() })
     }
 
-    public func storeSize(in size: Binding<CGSize?>, block: @escaping () -> Void) -> some View {
-        storeSize(in: size, block: { _ in block() })
+    public func storeSize(
+        update strategy: SizeUpdateStrategy = .once,
+        in size: Binding<CGSize?>,
+        block: @escaping () -> Void
+    ) -> some View {
+        storeSize(update: strategy, in: size, block: { _ in block() })
     }
 }
 
 extension View {
-    public func storeHeight(in height: Binding<CGFloat>, block: @escaping (CGFloat) -> Void = { _ in }) -> some View {
-        readHeight {
+    public func storeHeight(
+        update strategy: SizeUpdateStrategy = .once,
+        in height: Binding<CGFloat>,
+        block: @escaping (CGFloat) -> Void = { _ in }
+    ) -> some View {
+        readHeight(update: strategy) {
             height.wrappedValue = $0
             block($0)
         }
     }
 
-    public func storeHeight(in height: Binding<CGFloat?>, block: @escaping (CGFloat) -> Void = { _ in }) -> some View {
-        readHeight {
+    public func storeHeight(
+        update strategy: SizeUpdateStrategy = .once,
+        in height: Binding<CGFloat?>,
+        block: @escaping (CGFloat) -> Void = { _ in }
+    ) -> some View {
+        readHeight(update: strategy) {
             height.wrappedValue = $0
             block($0)
         }
     }
 
-    public func storeHeight(in height: Binding<CGFloat>, block: @escaping () -> Void) -> some View {
-        storeHeight(in: height, block: { _ in block() })
+    public func storeHeight(
+        update strategy: SizeUpdateStrategy = .once,
+        in height: Binding<CGFloat>,
+        block: @escaping () -> Void
+    ) -> some View {
+        storeHeight(update: strategy, in: height, block: { _ in block() })
     }
 
-    public func storeHeight(in height: Binding<CGFloat?>, block: @escaping () -> Void) -> some View {
-        storeHeight(in: height, block: { _ in block() })
+    public func storeHeight(
+        update strategy: SizeUpdateStrategy = .once,
+        in height: Binding<CGFloat?>,
+        block: @escaping () -> Void
+    ) -> some View {
+        storeHeight(update: strategy, in: height, block: { _ in block() })
     }
 }
 
 extension View {
-    public func storeWidth(in width: Binding<CGFloat>, block: @escaping (CGFloat) -> Void = { _ in }) -> some View {
-        readWidth {
+    public func storeWidth(
+        update strategy: SizeUpdateStrategy = .once,
+        in width: Binding<CGFloat>,
+        block: @escaping (CGFloat) -> Void = { _ in }
+    ) -> some View {
+        readWidth(update: strategy) {
             width.wrappedValue = $0
             block($0)
         }
     }
 
-    public func storeWidth(in width: Binding<CGFloat?>, block: @escaping (CGFloat) -> Void = { _ in }) -> some View {
-        readWidth {
+    public func storeWidth(
+        update strategy: SizeUpdateStrategy = .once,
+        in width: Binding<CGFloat?>,
+        block: @escaping (CGFloat) -> Void = { _ in }
+    ) -> some View {
+        readWidth(update: strategy) {
             width.wrappedValue = $0
             block($0)
         }
     }
 
-    public func storeWidth(in width: Binding<CGFloat>, block: @escaping () -> Void) -> some View {
-        storeWidth(in: width, block: { _ in block() })
+    public func storeWidth(
+        update strategy: SizeUpdateStrategy = .once,
+        in width: Binding<CGFloat>,
+        block: @escaping () -> Void
+    ) -> some View {
+        storeWidth(update: strategy, in: width, block: { _ in block() })
     }
 
-    public func storeWidth(in width: Binding<CGFloat?>, block: @escaping () -> Void) -> some View {
-        storeWidth(in: width, block: { _ in block() })
+    public func storeWidth(
+        update strategy: SizeUpdateStrategy = .once,
+        in width: Binding<CGFloat?>,
+        block: @escaping () -> Void
+    ) -> some View {
+        storeWidth(update: strategy, in: width, block: { _ in block() })
     }
 }
